1731f85e547158619b47c68bc7c40b72b8ed40ee
sinngoro456 <145282037+sinngoro456@users.noreply.github.com>
2024-10-19 15:50:39 +0900

    あ

--- GitHandler/AutoMatlabRunner.py
+++ GitHandler/AutoMatlabRunner.py
@@ -48,6 +48,7 @@ def check_matlab_process():
 
 def pull_repository(repo_path):
     print("pull_repository")
+    print("pull_repository")
     # リポジトリをプル
     os.chdir(repo_path)
     repo = git.Repo()
